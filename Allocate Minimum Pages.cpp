dc854a433a27fa79cf947c91077450fcccec2bb4
Ronit Jain <124519781+Ronit675@users.noreply.github.com>
2025-09-01 23:50:30 +0530

    Document Allocate Minimum Pages solution approach

--- Allocate Minimum Pages.cpp
+++ Allocate Minimum Pages.cpp
@@ -1,3 +1,25 @@
+// # Intuition
+// <!-- Describe your first thoughts on how to solve this problem. -->
+// -> First thought was to find that number which is smallest possible maximum.
+// -> The Range in which this number can lie is between the max of array and sum of array.
+
+// # Approach
+// <!-- Describe your approach to solving the problem. -->
+// -> To solve this using binary search we need to define the low and high.
+// -> the lowest possible number of pages can be maximum of array and highest can be the sum of array.
+// -> now create a function which will check how many students are required to take mid number of pages.
+// -> If the number of students is greater than the given k then move low = mid+1.
+// -> which means low is too less number for the number of pages to be assigned.
+// -> If the number of students is less or equal to k then move high = mid-1.
+// -> which means there are chances of another less number that may fall between low and mid-1.
+
+// # Complexity
+// - Time complexity:
+// O(logn)
+
+// - Space complexity:
+// O(1)
+
 class Solution {
   public:
     int check(vector<int> &arr, int n, int no){
